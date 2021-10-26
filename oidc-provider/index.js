e42fff9f4f744b84c67dec273871ed70bcfc6805
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-10-26 16:47:11 -0400

    Enables client credentials grant type for OIDC

--- oidc-provider/index.js
+++ oidc-provider/index.js
@@ -9,51 +9,62 @@ app.use(express.static(__dirname + '/public'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const rucioAuthClient = {
+    client_id: "client_01",
+    client_secret: "secret_01",
+    registration_access_token: "rat_01",
+    redirect_uris: [
+        "http://localhost:8080/auth/oidc_token",
+        "http://localhost:8080/auth/oidc_code",
+    ],
+    client_name: "rucio-auth-client",
+    token_endpoint_auth_method: "client_secret_basic",
+    scope: "address fts phone openid profile offline_access rucio email wlcg wlcg.groups fts:submit-transfer",
+    grant_types: [
+        "refresh_token",
+        "urn:ietf:params:oauth:grant-type:token-exchange",
+        "authorization_code"
+    ],
+    response_types: [
+        "code"
+    ],
+    jwksType: "URI",
+    default_max_age: 60000,
+    require_auth_time: true,
+    client_secret_expires_at: 0,
+}
+
+const rucioAdminClient = {
+    client_id: "client_02",
+    client_secret: "secret_02",
+    registration_access_token: "rat_02",
+    redirect_uris: [
+        "http://localhost:8080/auth/oidc_token",
+        "http://localhost:8080/auth/oidc_code",
+    ],
+    client_name: "rucio-admin-client",
+    token_endpoint_auth_method: "client_secret_basic",
+    scope: "address scim:read phone email wlcg profile fts:submit-transfer rucio fts fts:submit-transfer",
+    grant_types: [
+        "client_credentials"
+    ],
+    response_types: [],
+    jwksType: "URI",
+    default_max_age: 60000,
+    require_auth_time: true,
+    client_secret_expires_at: 0,
+}
+
+
 const configuration = {
-    clients: [
-        {
-            client_id: "client_01",
-            client_secret: "secret_01",
-            registration_access_token: "rat_01",
-            redirect_uris: [
-                "http://localhost:8080/auth/oidc_token",
-                "http://localhost:8080/auth/oidc_code",
-            ],
-            client_name: "rucio-auth-client",
-            token_endpoint_auth_method: "client_secret_basic",
-            scope: "address fts phone openid profile offline_access rucio email wlcg wlcg.groups fts:submit-transfer",
-            grant_types: [
-                "refresh_token",
-                "urn:ietf:params:oauth:grant-type:token-exchange",
-                "authorization_code"
-            ],
-            response_types: [
-                "code"
-            ],
-            jwksType: "URI",
-            default_max_age: 60000,
-            require_auth_time: true,
-            client_secret_expires_at: 0,
-        }, {
-            client_id: "client_02",
-            client_secret: "secret_02",
-            registration_access_token: "rat_02",
-            redirect_uris: [
-                "http://localhost:8080/auth/oidc_token",
-                "http://localhost:8080/auth/oidc_code",
-            ],
-            client_name: "rucio-admin-client",
-            token_endpoint_auth_method: "client_secret_basic",
-            scope: "address scim:read phone email wlcg profile fts:submit-transfer rucio fts fts:submit-transfer",
-            grant_types: [
-                "client_credentials"
-            ],
-            response_types: [],
-            jwksType: "URI",
-            default_max_age: 60000,
-            require_auth_time: true,
-            client_secret_expires_at: 0,
+    features: {
+        clientCredentials: {
+            enabled: true
         }
+    },
+    clients: [
+        rucioAuthClient,
+        rucioAdminClient
     ],
     pkce: {
         required: () => false,
